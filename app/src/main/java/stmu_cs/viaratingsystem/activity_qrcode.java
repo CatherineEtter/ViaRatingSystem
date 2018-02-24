6032820a0584944a5e56d4c27f4edacc6f96977f
Catherine Etter <cetter.catherine.web@gmail.com>
2018-02-24 10:02:47 -0600

    Data Passing
    QR code activity now passes data to Rating Activity
    Rating Activity has been added to manifest

--- app/src/main/java/stmu_cs/viaratingsystem/activity_qrcode.java
+++ app/src/main/java/stmu_cs/viaratingsystem/activity_qrcode.java
@@ -42,7 +42,11 @@ public class activity_qrcode extends AppCompatActivity {
                 Toast.makeText(this,"Scan cancelled",Toast.LENGTH_LONG).show();
             }
             else {
-                Toast.makeText(this,result.getContents(),Toast.LENGTH_LONG).show();
+               // Toast.makeText(this,result.getContents(),Toast.LENGTH_LONG).show();
+                String scanResult = result.getContents();
+                Intent intent = new Intent(getBaseContext(),RatingActivity.class);
+                intent.putExtra("data",scanResult);
+                startActivity(intent);
             }
         }
         else {
