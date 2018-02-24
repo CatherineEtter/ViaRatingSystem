e62e9a186d8293544dea42c45b54d64192d9168d
mchristman1 <35343850+mchristman1@users.noreply.github.com>
2018-02-23 18:26:11 -0600

    Added empty activities

---
+++ app/src/main/java/stmu_cs/viaratingsystem/DashboardActivity.java
@@ -0,0 +1,8 @@
+package stmu_cs.viaratingsystem;
+
+/**
+ * Created by mchri on 2/23/2018.
+ */
+
+public class DashboardActivity {
+}
---
+++ app/src/main/java/stmu_cs/viaratingsystem/UserModel.java
@@ -0,0 +1,44 @@
+package stmu_cs.viaratingsystem;
+
+/**
+ * Created by mchri on 2/23/2018.
+ */
+
+public class UserModel {
+    private String email;
+    private String userName;
+    private String password;
+    private int points;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
